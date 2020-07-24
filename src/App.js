e163fe68d86305d5f669fc81659bf0848a773264
ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2020-07-24 10:58:44 +0100

    Updated components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import { evaluate } from 'mathjs';
-import Buttons from './Buttons'
-import Display from './Display'
+import Buttons from './components/Buttons'
+import Display from "./components/Display"
 
 
 
